4434f9dab49f2ecaf893531ac30dbc675a6fd7cf
vzbel <142253332+vzbel@users.noreply.github.com>
2025-04-27 19:50:07 -0600

    Add current dynamic weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Dashboard from './Dashboard'
 import Sidebar from './Sidebar'
+
+
 function App() {
 
   return (
-    <div className="flex h-screen">
+    <div className="flex ">
       <Sidebar />
       <Dashboard />
     </div>
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,19 +1,37 @@
 import { SunIcon } from '@heroicons/react/24/outline';
 import PlaceCard from './PlaceCard';
+import { useEffect, useState } from 'react';
+
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
 export default function Dashboard(){
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(()=>{
+        async function getWeatherData(){
+            const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=El Paso&days=6&aqi=yes&alerts=no`);
+            const json = await response.json();
+            // console.log(json)
+            setWeatherData(json)
+        }
+        getWeatherData()
+    },[])
+
+
     return (
-        <main className="px-20 py-40 bg-gray-950 flex-1 text-white">
-            <div className="flex p-5 py-8 gap-4 bg-gray-900 max-w-lg flex-wrap items-center rounded-xl mb-24 shadow-sm">
-                <SunIcon className="text-yellow-400 max-w-32"/>
+        weatherData ?
+        <main className="px-20 py-40 bg-gray-900 flex-1 text-white">
+            <div className="flex p-5 py-8 gap-4 bg-gray-800 max-w-lg flex-wrap items-center rounded-xl mb-24 shadow-sm">
+            <img className="flex-1" src={weatherData.current.condition.icon} alt="" />
                 <div className="flex-1">
-                    <div className="flex items-center mb-2 gap-2">
-                        <p className="text-4xl">71&deg;</p>
-                        <p className="text-2xl">Clear</p>
+                    <div className="flex items-center mb-2">
+                        <p className="text-4xl">{weatherData.current.temp_f}&deg;</p>
+                        <span className="text-xs text-gray-300">F</span>
+                        <p className="ml-3 text-2xl">{weatherData.current.condition.text}</p>
                     </div>
-                    <p>El Paso, Texas</p>
-                    <p className="text-gray-400">AQI: 65</p>
-                    <p className="text-gray-400">CO: 45</p>
+                    <p>{weatherData.location.name}, {weatherData.location.region}</p>
+                    <p className="text-gray-400">Wind: {weatherData.current.wind_mph} mph</p>
+                    <p className="text-gray-400">Cloud: {weatherData.current.cloud} %</p>
                 </div>
             </div>
 
@@ -27,5 +45,7 @@ export default function Dashboard(){
                 </div>
             </div>
         </main>
+        :
+        <>Loading...</>
     );
 }
