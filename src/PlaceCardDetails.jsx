58ab89baf4c556288aabe49ce9ca55d263a4bce6
vzbel <142253332+vzbel@users.noreply.github.com>
2025-05-01 13:24:10 -0600

    Fix homepage link

--- src/PlaceCardDetails.jsx
+++ src/PlaceCardDetails.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 
+import Sidebar from './Sidebar.jsx'
+
 // Detail page for place card,
 // contains image, name, address, website info, etc.
 export default function PlaceCardDetails(){
@@ -21,20 +23,23 @@ export default function PlaceCardDetails(){
     place ? console.log(place.websiteURL) : <></>
     return (
         place ?
-        <article className="flex items-center gap-10 max-w-4xl mx-auto py-36">
-            {/* Image for place */}
-            <img src={place.imgURL} alt="" className="max-w-1/2"/>
-            {/* Place data like name, description, etc */}
-            <div className="grow">
-                <div className="mb-4">
-                    <p className="text-white text-2xl">{place.name}</p>
-                    <p className="text-gray-400">{place.address}</p>
+        <div className="flex">
+            <Sidebar />
+            <article className="flex items-center gap-10 max-w-4xl mx-auto py-36 flex-wrap">
+                {/* Image for place */}
+                <img src={place.imgURL} alt="" className="grow"/>
+                {/* Place data like name, description, etc */}
+                <div className="grow">
+                    <div className="mb-4">
+                        <p className="text-white text-2xl">{place.name}</p>
+                        <p className="text-gray-400">{place.address}</p>
+                    </div>
+                    <p className="text-gray-400">{place.description}</p>
+                    {/* Link to place's website */}
+                    <Link to={place.website} className="bg-blue-400 text-white my-5 block max-w-32 px-4 py-2">Visit Website</Link>
                 </div>
-                <p className="text-gray-400">{place.description}</p>
-                {/* Link to place's website */}
-                <Link to={place.website} className="bg-blue-400 text-white my-5 block max-w-32 px-4 py-2">Visit Website</Link>
-            </div>
-        </article>
+            </article>
+        </div>
         :
         <p>Loading...</p>
     );
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar(){
     return (
         <nav className="flex-1 py-10 px-3 bg-gray-900 text-white max-w-3xs">
             <h1 className="flex items-center mb-6"><SunIcon className="w-8 mr-2"/>EPWeather</h1>
-            <Link className="bg-gray-800 p-3 border-l-2 text-sm block">Today's Forecast</Link>
+            <Link to="/" className="bg-gray-800 p-3 border-l-2 text-sm block">Today's Forecast</Link>
         </nav>
     );
 }
